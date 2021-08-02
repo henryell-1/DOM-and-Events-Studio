81c19219d61a11fe61caa7d6e8199a18e0b93e00
henryell <henrydell4793@gmail.com>
2021-08-01 21:24:33 -0500

    commit first

--- scripts.js
+++ scripts.js
@@ -1,2 +1,61 @@
 // Write your JavaScript code here.
+window.addEventListener("load", function() {
+    // put DOM code here to ensure elements have been loaded
+    console.log('window loaded');
+    window.addEventListener("load", function() {
+        // put DOM code here to ensure elements have been loaded
+        console.log('window loaded');
+
+ let response = window.confirm("Confirm that the shuttle is ready for takeoff." );
+// Code does NOT continue until user responds to confirm window
+if (response) {
+    let flightStatus = document.getElementById("takeoff-button");
+    flightStatus.addEventListener("click", function (event) {
+        console.log("Shuttle in flight.");
+    });
+
+let div = document.getElementById("shuttleBackground");
+console.log("div contents:", div.innerHTML.fullScreen());
+         div.style.color = "blue";
+
+
+let p = document.getElementById("spaceShuttleHeight");
+   console.log(p.innerHTML);
+
+}else
+let response = window.confirm("The shuttle is landing. Landing gear engaged." ); 
+if (response) {
+    let flightStatus = document.getElementById("Landing-button");
+    flightStatus.addEventListener("click", function (event) {
+        console.log("The shuttle has landed.");
+    });
+
+    let div = document.getElementById("shuttleBackground");
+    console.log("div contents:", div.innerHTML.fullScreen());
+             div.style.color = "green";
+
+   let p = document.getElementById("spaceShuttleHeight");
+   console.log(p.innerHTML);{
+   
+   }
+}else
+let response = window.confirm( "Confirm that you want to abort the mission." );
+if (response) {
+    let flightStatus = document.getElementById("missionAbort-button");
+    flightStatus.addEventListener("click", function (event) {
+        console.log("Mission aborted.");
+    });
+
+    let div = document.getElementById("shuttleBackground");
+    console.log("div contents:", div.innerHTML.fullScreen());
+             div.style.color = " ";
+
+   let p = document.getElementById("spaceShuttleHeight");
+   console.log(p.innerHTML);{
+   
+   }
+}
+
+});
+    });
 // Remember to pay attention to page loading!
